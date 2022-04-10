38af0df66f12cfc34c204c8ea0e25b600a4f0aa6
XJXJ <13725391028@163.com>
2022-04-11 04:59:52 +0800

    Update On April 11st

--- Orders.py
+++ Orders.py
@@ -1,10 +1,22 @@
+
+import sys
+sys.path.append('C:\\Users\\Administrator\\AppData\\Local\\Programs\\Python\\Python37\\Lib')
+sys.path.append('C:\\Users\\Administrator\\AppData\\Local\\Programs\\Python\\Python37\\Lib\\site-packages')
+
 import os
 from PyQt5.QtGui import QColor
 from PyQt5.QtCore import Qt
-import sys
-from functools import *
-from PyQt5 import QtWidgets
-from PyQt5.QtCore import Qt,QModelIndex,QItemSelectionModel,pyqtSignal,QMimeData,Qt
-from PyQt5.QtGui import QStandardItemModel, QStandardItem,QDrag
+from PyQt5 import QtGui
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+
+
+import cv2.cv2 as cv2
+import numpy as np
+from PIL import Image
+
+from functools import *
+
 
